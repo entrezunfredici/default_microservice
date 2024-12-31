d9205662c12de766fddc554482025b3f7f42e72f
entrezunfredici <superfred2468@gmail.com>
2024-12-31 13:39:07 +0100

    [DEL] seeder content

--- seeder.js
+++ seeder.js
@@ -28,30 +28,6 @@ const seedDatabase = async () => {
 
     await db.instance.query("PRAGMA foreign_keys = OFF");
 
-    const users = require("./seeds/User.seed.json");
-    users.forEach((user) => {
-      user.password = bcrypt.hashSync(user.password, 10);
-    });
-    await db.User.bulkCreate(users);
-    console.log("Users table seeded successfully");
-
-    const roles = require("./seeds/Role.seed.json");
-    await db.Role.bulkCreate(roles);
-    console.log("Roles table seeded successfully");
-
-    const subjects = require("./seeds/Subject.seed.json");
-    await db.Subject.bulkCreate(subjects);
-    console.log("Subjects table seeded successfully");
-
-    await db.LeitnerSystem.bulkCreate(
-      require("./seeds/LeitnerSystem.seed.json")
-    );
-    console.log("LeitnerSystems table seeded successfully");
-    await db.LeitnerBox.bulkCreate(require("./seeds/LeitnerBox.seed.json"));
-    console.log("LeitnerBoxes table seeded successfully");
-    await db.LeitnerCard.bulkCreate(require("./seeds/LeitnerCard.seed.json"));
-    console.log("LeitnerCards table seeded successfully");
-    await db.instance.query("PRAGMA foreign_keys = ON");
 
     console.log("Sample data inserted");
   } catch (error) {
