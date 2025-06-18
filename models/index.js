b6ae0b145634dd6aefb4c1a9b54e19f1bf4a0c0e
entrezunfredici <superfred2468@gmail.com>
2025-06-18 12:04:52 +0200

    [ADD] dbms

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize");
+const dbmsConfig = require("../config/dbms.config");
 const dbConfig = require("../config/db.config");
 
 // Création de l'instance Sequelize
-const instance = new Sequelize(dbConfig);
+const instance = new Sequelize(process.env.ENVIRONMENT === "prod" ? dbmsConfig : dbConfig);()
 
 // Models
 const models = {};
@@ -13,6 +14,16 @@ Object.keys(models).forEach((modelName) => {
   if (models[modelName].associate) {
     models[modelName].associate(models);
   }
+});
+
+// instance.sync({ force: true }).then(() => {
+//   console.log("reset database success"); 
+// });
+
+instance.sync({ force: true }).then(() => {
+  console.log("Base de données synchronisée (force true)");
+}).catch((err) => {
+  console.error("Erreur lors de la synchronisation de la base :", err);
 });
 
 module.exports = {
